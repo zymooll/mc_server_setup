e741d3fdca0f9c96c6fec82fb0079fbd3f574882
zymooll <51841832+zymooll@users.noreply.github.com>
2020-11-28 03:30:06 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 webh="http://ddns.myddns.me:3421/"
 bi=("" "zfile/directlink/7/craftbukkit/craftbukkit-1.7.9-R0.2-SNAPSHOT.jar" "zfile/file/7/craftbukkit/craftbukkit-1.8.8-R0.1-SNAPSHOT-latest.jar" "zfile/directlink/7/craftbukkit/craftbukkit-1.12.2.jar" "zfile/directlink/7/craftbukkit/craftbukkit-1.14.4-R0.1-SNAPSHOT.jar" "zfile/directlink/7/Spigot/spigot-1.7.9-R0.2-SNAPSHOT.jar" "zfile/directlink/7/Spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar" "zfile/directlink/7/Spigot/Spigot-1.12.2.jar" "zfile/directlink/7/Spigot/Spigot-1.15.1.jar")
 download(){
-    wget [[${webh}+${bi[$1]}]]
+    wget ${webh}${bi[$1]}
     echo "下载完毕"
 }
 start:
@@ -11,35 +11,37 @@ echo "欢迎使用本自动开服程序"
 echo "1)下载服务器端"
 echo "2)开启服务器端"
 read -p ">" -n 1 -s inp
-if ((inp==1));then
+if ((${inp}==1));then
     start1:
     echo "1)CraftBukkit"
     echo "2)Spigot"
     read -p ">" -n 1 -s inp
-    if ((inp==1));then
+    if ((${inp}==1));then
         start2:
         echo "1)1.7.9"
         echo "2)1.8.8"
         echo "3)1.12.2"
         echo "4)1.14.4"
         read -p ">" -n 1 -s inp
-        if ((inp<5));then 
-            download ((inp-1))
+        if ((${inp}<5));then 
+            let ${inp}-1
+            download ${inp}
             break
         else
             echo "输入有误"
             goto start2
         fi
         break
-    elif ((inp==2));then
+    elif ((${inp}==2));then
         start3:
         echo "1)1.7.9"
         echo "2)1.8.8"
         echo "3)1.12.2"
         echo "4)1.14.4"
         read -p ">" -n 1 -s inp
-        if ((inp<5));then
-            download ((inp+3))
+        if ((${inp}<5));then
+            let ${inp}+3
+            download ${inp}
             break
         else
             echo "输入有误"
@@ -51,7 +53,7 @@ if ((inp==1));then
         goto start1
     fi
     break
-elif ((inp==2));then
+elif ((${inp}==2));then
     echo "开服程序正在完成初始化..."
     yum install java
     yum install screen
